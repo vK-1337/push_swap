61add2b9f48f3c099ba003024c9edb3676a1c0b1
Vincent DA CONCEICAO RAMOS <vince9322@gmail.com>
2023-12-23 09:58:27 +0100

    code moves

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: vk <vk@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 08:40:29 by vda-conc          #+#    #+#             */
-/*   Updated: 2023/12/22 23:47:31 by vk               ###   ########.fr       */
+/*   Updated: 2023/12/23 09:56:11 by vk               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,7 @@ void ft_push_a_node(t_list *node, t_list *target_node, t_list **list_a, t_list *
     if (node->position < node_list_size / 2 && target_node->position < target_list_size / 2)
     {
       while (node->position > 1 && target_node->position > 1)
-        ft_swap_rr(list_a, list_a);
+        ft_swap_rr(list_a, list_b);
     }
     else if (node->position > node_list_size / 2 && target_node->position > target_list_size / 2)
     {
@@ -67,14 +67,18 @@ void ft_push_a_node(t_list *node, t_list *target_node, t_list **list_a, t_list *
     if (node->position <= node_list_size / 2)
     {
       while (node->position > 1)
+      {
         ft_swap_ra(list_a, 0);
+      }
     }
     else
     {
       if (node->position - 1 >= node->position - node_list_size && node->position != ft_lstsize(*list_a))
       {
         while (node->position > 1)
+        {
           ft_swap_ra(list_a, 0);
+        }
       }
       else
       {
@@ -105,6 +109,8 @@ void ft_push_swap(t_list **list_a)
   t_list *target_node;
 
 	list_b = malloc(sizeof (t_list *));
+  list_b = NULL;
+  pause();
 	while (ft_lstsize(*list_a) > 3 && ft_lstsize(list_b) < 2)
 		ft_swap_pb(list_a, &list_b);
 	while (ft_lstsize(*list_a) > 3)
